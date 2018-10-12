5bc9f0439e5429aa00cc8d8d15e550183bc34d9b
anumeha1986 <43923735+anumeha1986@users.noreply.github.com>
2018-10-12 19:25:38 +0530

    Add files via upload

---
+++ myFirstOutput.java
@@ -0,0 +1,11 @@
+package myWorkspaceJavaPractice;
+
+public class myFirstOutput {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("My first program");
+		System.out.println("Anumeha");
+	}
+
+}
